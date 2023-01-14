32820e9562adec646588203fc5b7c273e232d5bc
tuannurlaila K <tuannurlaila.k@ku.th>
2023-01-14 11:33:44 +0700

    first

---
+++ page1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="show.php" method="get">
+    Name:<input type="text" name="name"><br>
+    Email:<input type="text"name="email"><br>
+    <input type="submit" value="send">
+</form>
+
+</body>
+</html>
---
+++ show.php
@@ -0,0 +1,5 @@
+<?php
+    $name=$_GET["name"];
+    $email=$_GET["email"];
+    echo "Name=$name, Email=$email";
+?>
