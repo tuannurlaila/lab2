803e47fcd91c38a71d3b06c6cdb4a557cef54291
Tuannurlaila katamo <tuannurlaila.k@ku.th>
2023-01-20 21:40:26 +0700

    test

--- page1.php
+++ page1.php
@@ -1,12 +1,29 @@
 <!DOCTYPE html>
 <html>
+
 <body>
 
-<form action="show.php" method="get">
-    Name:<input type="text" name="name"><br>
-    Email:<input type="text"name="email"><br>
-    <input type="submit" value="send">
-</form>
+    <h1>แบบสอบถาม</h1>
+    <form action="show.php" method="get" style="margin-left: 80px;">
+    ชื่อ:<input type="text" name="name"><br>
+       
+        อายุ:
+        <input type="number" name="num" style="width: 40px;"><br>
+        
+        สถานะ:
+        <select name="status">
+            <option value="single">โสด</option>
+            <option value="marry">สมรส</option>
+        </select><br>
 
+        เพศ:
+        <select name="gender">
+            <option value="male">ชาย</option>
+            <option value="female">หญิง</option>
+            <option value="female">ไม่ระบุเพศ</option>
+        </select><br>
+
+        <input type="submit" value="ส่ง">
+    </form>
 </body>
 </html>
