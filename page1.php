7c2eab142cc5a08183c989e6f678c7ae032d2927
Tuannurlaila katamo <tuannurlaila.k@ku.th>
2023-01-20 21:52:58 +0700

    test

--- page1.php
+++ page1.php
@@ -5,18 +5,19 @@
 
     <h1>แบบสอบถาม</h1>
     <form action="show.php" method="get" style="margin-left: 80px;">
-    ชื่อ:<input type="text" name="name"><br>
+    ชื่อ-สกุล :
+    <input type="text" name="name"><br>
        
-        อายุ:
+        อายุ :
         <input type="number" name="num" style="width: 40px;"><br>
         
-        สถานะ:
+        สถานะ :
         <select name="status">
             <option value="single">โสด</option>
             <option value="marry">สมรส</option>
         </select><br>
 
-        เพศ:
+        เพศ :
         <select name="gender">
             <option value="male">ชาย</option>
             <option value="female">หญิง</option>
